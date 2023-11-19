c8377bff3750e3205a53673adae3aed020440027
Rui Dias <rui.ldias@ceiia.com>
2023-11-19 14:35:44 +0000

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,56 @@
+import Mapbox from "@/components/mapbox";
+
+export default async function Home() {
+    const portugalDistricts: any = [];
+    const districtBoundaries: any = [];
+    let portugalList = undefined;
+    let portugalGeojson = undefined;
+    const string = `http://localhost:3001/assets/datasets/portugal/Portugal.geojson`;
+
+    const response = await fetch(
+        "http://localhost:3001/assets/datasets/fileList_PT.json"
+    );
+    const result = await response.json();
+    portugalList = result;
+
+    const requestPortugalGeojson = await fetch(string, {
+        cache: "no-store",
+    });
+
+    portugalGeojson = await requestPortugalGeojson.json();
+
+    const getDistricts = async () => {
+        await Promise.all(
+            result.districts.map(async (file: any, index: any) => {
+                const actualFile = `http://localhost:3001/assets/datasets/portugal/${file.name}`;
+
+                portugalDistricts.push({
+                    name: file.name,
+                    selected: file.selected,
+                });
+
+                if (file.selected) {
+                    const response = await fetch(actualFile, {
+                        cache: "no-store",
+                    });
+                    const responseData = await response.json();
+
+                    districtBoundaries.push(responseData);
+                }
+            })
+        );
+    };
+    // Fetch and render district boundaries
+    await getDistricts();
+
+    return (
+        <main className="w-full h-full">
+            <Mapbox
+                portugalDistricts={portugalDistricts}
+                districtBoundaries={districtBoundaries}
+                portugalList={portugalList}
+                portugalGeojson={portugalGeojson}
+            />
+        </main>
+    );
+}

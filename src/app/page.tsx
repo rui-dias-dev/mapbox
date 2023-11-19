185b55ee5f342ba095c3a6d8058d44b205a05b0c
Rui Dias <rui.ldias@ceiia.com>
2023-11-19 19:55:56 +0000

    added mapbox component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,14 @@
+import MapComponent from "@/components/map-components";
 import Mapbox from "@/components/mapbox";
 import { Suspense } from "react";
 
 export default async function Home() {
-    const portugalDistricts: any = [];
-    const districtBoundaries: any = [];
-    let portugalList = undefined;
-    let portugalGeojson = undefined;
-    const string = `https://assets-geojson.vercel.app/assets/datasets/portugal/Portugal.geojson`;
-
-    const response = await fetch(
-        "https://assets-geojson.vercel.app/assets/datasets/fileList_PT.json"
-    );
-    const result = await response.json();
-    portugalList = result;
-
-    const requestPortugalGeojson = await fetch(string, {
-        cache: "no-store",
-    });
-
-    portugalGeojson = await requestPortugalGeojson.json();
-
-    const getDistricts = async () => {
-        await Promise.all(
-            result.districts.map(async (file: any, index: any) => {
-                const actualFile = `https://assets-geojson.vercel.app/assets/datasets/portugal/${file.name}`;
-
-                portugalDistricts.push({
-                    name: file.name,
-                    selected: file.selected,
-                });
-
-                if (file.selected) {
-                    const response = await fetch(actualFile, {
-                        cache: "no-store",
-                    });
-                    const responseData = await response.json();
-
-                    districtBoundaries.push(responseData);
-                }
-            })
-        );
-    };
-    // Fetch and render district boundaries
-    await getDistricts();
+ 
 
     return (
         <main className="w-full h-full">
             <Suspense fallback={<div>loading...</div>}>
-                <Mapbox
-                    portugalDistricts={portugalDistricts}
-                    districtBoundaries={districtBoundaries}
-                    portugalList={portugalList}
-                    portugalGeojson={portugalGeojson}
-                />
+                <MapComponent />
             </Suspense>
         </main>
     );
